ab7698bc1153c26a9a13cdf95c962c69be9a0503
hyeokjun <hyeokjun@snu.ac.kr>
2026-06-18 01:35:54 +0900

    Initial Commit

---
+++ atmos-adminTests/atmos_adminTests.swift
@@ -0,0 +1,18 @@
+//
+//  atmos_adminTests.swift
+//  atmos-adminTests
+//
+//  Created by hyeokjun on 6/18/26.
+//
+
+import Testing
+
+struct atmos_adminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
